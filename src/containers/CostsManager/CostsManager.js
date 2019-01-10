e317ca1d391e5ebb8cdb6e27551f52f5650ba10e
mkubibaev <mkubibaev@gmail.com>
2019-01-10 15:22:58 +0600

    added main container

---
+++ src/containers/CostsManager/CostsManager.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class CostsManager extends Component {
+    state = {
+
+    };
+
+    render() {
+        return (
+            <div>123</div>
+        )
+    }
+}
+
+export default CostsManager;
